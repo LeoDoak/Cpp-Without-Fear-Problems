7fc14ae151edbeb95bbb23578abd6a7c07cb021a
Leo Doak <125704724+LeoDoak@users.noreply.github.com>
2024-12-21 10:55:01 -0500

    dealer

---
+++ chap_5/bagpicknum.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+using namespace std;
+
+// Excercise 5.3.3 pg 126 
+
+int rand_0toN1(int n); 
+void bag_pick();   
+
+char *color[4] = {"red", "blue", "orange", "green"}; 
+char *item[2] = {"ball", "cube"}; 
+
+int main(){
+	int n, i; 
+	srand(time(NULL)); 
+
+	while(1) { 
+		cout << "1 to continue, (0 to exit): ";
+		cin >> n; 
+		if (n == 0)
+			break; 
+		for (i = 1; i <= 4; i++) 
+			bag_pick(); 
+	}
+	return 0;
+
+}
+
+void bag_pick() { 
+	int c; 
+	int i; 
+	int num = rand_0toN1(8);
+
+	c = num % 4; // remainder for color 
+	i = num / 4; // either 0 or 1 
+	cout << color[c] << " " <<  item[i] << endl; 
+}
+
+int rand_0toN1(int n){
+	return rand() % n; 
+}
