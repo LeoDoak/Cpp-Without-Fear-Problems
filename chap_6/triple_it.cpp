e8aa29be5cf25db37329ed3d8ea4d67af18b1ef6
Leo Doak <125704724+LeoDoak@users.noreply.github.com>
2024-12-22 00:27:12 -0500

    update

---
+++ chap_6/triple_it.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+void triple_it(int *p); 
+
+int main() {
+	
+	int n = 15; 
+	cout << "The value of a before tripling: " << n << endl; 
+	triple_it(&n);
+	cout << "The value of b after tripling: " << n << endl;
+	return 0;  
+}
+
+void triple_it(int *p){
+	*p = *p * 3; 
+}
